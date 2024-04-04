c8881ba40f6b33533ed4490448caf20eef1a6855
yojimat <31702569+yojimat@users.noreply.github.com>
2024-04-04 10:39:46 -0300

    Use data caching for performance improvements

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import bodyParser from 'body-parser';
 import v1WourkoutRouter from "./v1/routes/workoutRoutes.js";
+import apicache from 'apicache';
 
 const app = express();
+const cache = apicache.middleware;
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cache("2 minutes"));
 app.use("/api/v1/workouts", v1WourkoutRouter);
 
 app.get("/", (_, res) => res.redirect(`/api/v1/workouts`));
